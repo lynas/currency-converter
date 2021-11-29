a1875fde313ccb2b5cedf9fbed350ec794fcfda1
lynas <szlynas@gmail.com>
2021-11-29 13:06:27 +0600

    Add pom file

--- src/main/java/com/lynas/service/ExternalApiService.java
+++ src/main/java/com/lynas/service/ExternalApiService.java
@@ -2,7 +2,9 @@ package com.lynas.service;
 
 import com.lynas.dto.ConversionResponseDTO;
 import com.lynas.dto.ProviderDTO;
+import com.lynas.exception.ApiRequestException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -27,8 +29,12 @@ public class ExternalApiService {
     }
 
     public Mono<ConversionResponseDTO> convertCurrency(double amount, String fromCurrency, String toCurrency){
-        return this.webClient.get().uri(apiBaseUrl+"/convert?access_key="+apiKey+"&from="+fromCurrency+"&to="+toCurrency+"&amount="+amount)
-                .retrieve().bodyToMono(ConversionResponseDTO.class);
+        return this.webClient.get()
+                .uri(apiBaseUrl+"/convert?access_key="+apiKey+"&from="+fromCurrency+"&to="+toCurrency+"&amount="+amount)
+                .retrieve()
+                .onStatus(status -> status.value() == HttpStatus.FORBIDDEN.value(),
+                        response -> Mono.error(new ApiRequestException("Invalid api key, Make sure api key has permission to call this API")))
+                .bodyToMono(ConversionResponseDTO.class);
     }
 
 }
